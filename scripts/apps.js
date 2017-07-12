74046b24198c52af7b63456d74077ce061ba16f4
sniebauer <sniebauer@gmail.com>
2017-07-11 20:17:00 -0500

    Added first JS commit

---
+++ scripts/apps.js
@@ -0,0 +1,12 @@
+window.onload = function(event) {
+  console.log('Hi mom');
+  console.log(document);
+  let ul = document.createElement('ul');
+  console.log(ul);
+
+  document.getElementByID('list').innerHTML = 'i like dogs';
+
+
+
+
+}
